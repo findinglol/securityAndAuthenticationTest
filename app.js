5f20b55027339df44da307832df840aeda2a1870
Mayank Kakkar <mayankkakkar1729@gmail.com>
2020-12-26 11:35:58 +0530

    Add Password encryption using mongoose-encrypt

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/userDB', { useUnifiedTopology: true, useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "Mayankwantstogetintouberassde.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render('home');
+});
+
+app.get("/login", (req, res) => {
+    res.render('login');
+});
+
+app.get("/register", (req, res) => {
+    res.render('register');
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+    const getEmail = req.body.username;
+    const getPassword = req.body.password;
+
+    User.findOne({ email: getEmail }, (err, foundUser) => {
+        if (err) {
+            res.send(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === getPassword) {
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log('Server running on Port Number 3000');
+});
